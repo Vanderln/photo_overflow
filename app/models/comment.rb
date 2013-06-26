3389b54bc6148f9c2aaaee7ad09d068a771f0334
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 15:32:04 -0500

    created user-comments association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   has_many :votes, as: :votable
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
+  has_many :comments
 end
