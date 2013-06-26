2b030b22d869462fbe1ca26312ef82ec277ab8bd
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 15:29:04 -0500

    created user-answers association

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,4 @@
 class Answer < ActiveRecord::Base
   has_many :votes, as: :votable
-
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_many :questions
+  has_many :answers
 end
