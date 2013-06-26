d9307040f39990a6d50b37bf8ae0bcec518552c6
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 14:08:12 -0500

    Vote controller, model, migration

---
+++ db/migrate/20130626190521_create_votes_table.rb
@@ -0,0 +1,10 @@
+class CreateVotesTable < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.integer :votable_id
+      t.string :votable_type
+      t.integer :user_id
+      t.integer :direction
+    end
+  end
+end
