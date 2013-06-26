849e87e74dee8fea145281d44f61110deb72926e
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 12:57:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoOverflow::Application.config.secret_token = 'ae18f727436b3fb695a845775134ddb9264ae492478a680463c98d4d629ff7bb5c5c5891b36b14603c6a6c40b5ba72295c75be8899f41e467fc5cfdf01b36c76'
