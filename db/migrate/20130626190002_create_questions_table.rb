a1f47bee2434eef09ad954b1cf85b33359fa6b06
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 14:04:39 -0500

    Question controller, model, migration

---
+++ db/migrate/20130626190002_create_questions_table.rb
@@ -0,0 +1,11 @@
+class CreateQuestionsTable < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :file
+      t.string :content
+      t.string :path
+    end
+  end
+end
