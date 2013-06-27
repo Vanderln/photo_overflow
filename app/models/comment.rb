6c5f0805f6cbc438e26a6fa29e00aa017c5714f9
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-27 17:17:01 -0500

    refactor create answer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :user_id, :content
+  attr_accessible :user_id, :content, :commentable_id, :commentable_type
   has_many :votes, as: :votable
   belongs_to :user
   belongs_to :commentable, polymorphic: true
