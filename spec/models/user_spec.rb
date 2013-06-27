e85aef4a29a7acbeaaadbc41ba0722eaa2f5465b
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-27 16:34:29 -0500

    added validation of presence test for username and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,10 +17,11 @@ describe User do
     it { should allow_mass_assignment_of(:email) }
   end
 
-
   context "testing validations" do
     # it { should validate_uniqueness_of(:email).case_insensitive }
     # it { should validate_uniqueness_of(:username).case_insensitive }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
     it { should_not allow_value("blah").for(:email) }
     it { should allow_value("a@b.com").for(:email) }
