cc065b777c8018857f30fdaee18b18f155e8cad6
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 22:22:48 -0500

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   has_many :votes
   attr_accessible :username, :email, :password
   has_secure_password
+
+  validates_uniqueness_of :email, :username
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
 end
