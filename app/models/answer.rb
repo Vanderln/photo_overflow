1d95728cb1e27a02a886e4d736b8a48e1ed87a14
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 22:22:55 -0500

    created answer view, changed model, migration

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,9 @@
 class Answer < ActiveRecord::Base
+  attr_accessible :content, :file
   has_many :votes, as: :votable
   has_many :comments, as: :commentable
   belongs_to :user
+  belongs_to :question
+
+  mount_uploader :file, PhotoUploader
 end
