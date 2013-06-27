4b616a03c3364173b436371834b4ea41dc6c667d
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-27 11:37:32 -0500

    removed duplicate attr_accessible line

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :comments
   has_many :votes
-  attr_accessible :username, :email, :password
+
   has_secure_password
 
   validates_uniqueness_of :email, :username
