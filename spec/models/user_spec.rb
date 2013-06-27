f7b18a1ceba3da736d614228cd945efa132fa477
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-27 15:59:34 -0500

    created tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,53 @@ require 'spec_helper'
 
 describe User do
 
-  context "validations" do
+  context "testing associations" do
+    it { should have_many(:questions) }
+    it { should have_many(:answers) }
+    it { should have_many(:comments) }
+    it { should have_many(:votes) }
+  end
+
+  context "testing attr_accessible" do
+      it "should not allow access to file" do
+        expect do
+          User.new(file: true)
+        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+
+      it "should not allow access to reputation" do
+        expect do
+          User.new(reputation: true)
+        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+
+      it "should not allow access to location" do
+        expect do
+          User.new(location:  true)
+        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+
+      it "should allow access to username" do
+        expect do
+          User.new(username:  true)
+        end.to_not raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+
+      it "should allow access to password" do
+        expect do
+          User.new(password:  true)
+        end.to_not raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+
+      it "should allow access to email" do
+        expect do
+          User.new(email: true)
+        end.to_not raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+  end
+
+
+  context "testing validations" do
 
     it "creates a user with a valid email address" do
       user = FactoryGirl.build(:user, email: "goodemail@gmail.com")
@@ -25,7 +71,5 @@ describe User do
       user2 = FactoryGirl.build(:user, username: "testusername")
       user2.should_not be_valid
     end
-
   end
-
 end
