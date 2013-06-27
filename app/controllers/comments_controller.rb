62f68bb7df98420d6c0f748cbc99ce091862948e
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-27 14:58:37 -0500

    built comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+  def index
+    @commentable = find_commentable
+    @comments = @commentable.comments
+  end
+
+  def create
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(params[:comment])
+    if @comment.save
+      flash[:notice] = "Successfully created comment"
+      redirect_to :id => nil
+    else
+      render :action => 'new'
+    end
+  end
+
+private
+
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
+  end
 end
