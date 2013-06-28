cfc3b5f219061bcc6aa48d11452f8aee41a747c0
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-27 20:14:11 -0500

    Refactoring question specs for model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -11,7 +11,6 @@ describe Question do
     it { should belong_to(:user) }    
   end
   
-
   context "testing attr_accessible" do
     it { should allow_mass_assignment_of(:file) }
     it { should allow_mass_assignment_of(:title) }
@@ -19,26 +18,4 @@ describe Question do
     it { should allow_mass_assignment_of(:path) }
     it { should allow_mass_assignment_of(:user_id) }
   end
-
-  # context "testing validations" do
-  #   # it { should validate_uniqueness_of(:email).case_insensitive }
-  #   # it { should validate_uniqueness_of(:username).case_insensitive }
-  #   it { should validate_presence_of(:username) }
-  #   it { should validate_presence_of(:email) }
-  #   it { should validate_presence_of(:password) }
-  #   it { should_not allow_value("blah").for(:email) }
-  #   it { should allow_value("a@b.com").for(:email) }
-
-  #   it "does not create a user, if the email has already been taken" do
-  #     user1 = FactoryGirl.create(:user, email: "goodemail@test.com")
-  #     user2 = FactoryGirl.build(:user, email: "goodemail@test.com")
-  #     user2.should_not be_valid
-  #   end
-
-  #   it "does not create a user, if the username has already been taken" do
-  #     user1 = FactoryGirl.create(:user, username: "testusername")
-  #     user2 = FactoryGirl.build(:user, username: "testusername")
-  #     user2.should_not be_valid
-  #   end
-  # end
 end
