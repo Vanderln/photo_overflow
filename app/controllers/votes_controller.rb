c51c4f8bed7dd457855fade0a557b3cbf98d05e3
Jason Benn <jasoncbenn@gmail.com>
2013-06-28 14:11:16 -0500

    voting with AJAX

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,14 @@
 class VotesController < ApplicationController
   def create
     if current_user
-      Vote.create(
+      vote = Vote.create(
         votable_id: params[:votable_id],
         votable_type: params[:votable_type],
         direction: params[:direction], 
         user_id: current_user.id)
+
+      votes_count = vote.votable.votes_count
+      render json: { votes_count: votes_count }
     end
-    redirect_to :back
   end
 end
